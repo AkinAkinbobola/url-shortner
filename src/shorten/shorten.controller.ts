16c098a356155bd34f3f56d097cf0275ef5c3128
Akin Akinbobola <akinbobola71@gmail.com>
2024-07-02 18:14:07 +0100

    Remove prefix

--- src/shorten/shorten.controller.ts
+++ src/shorten/shorten.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ShortenService } from './shorten.service';
 import { GetShortUrlDto } from './dtos/get-short-url.dto';
 
-@Controller('shorten')
+@Controller('')
 export class ShortenController {
   constructor(private readonly shortenService: ShortenService) {
   }
